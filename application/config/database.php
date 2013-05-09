782e6fdce87cd1aab507100f0dea8cc47a0e183a
Marc-Fran?ois Cochaux-Laberge <mfcochauxlaberge@gmail.com>
2013-05-09 12:23:32 -0400

    Fixed database config file

--- application/config/database.php
+++ application/config/database.php
@@ -70,9 +70,9 @@ return array(
 		'mysql' => array(
 			'driver'   => 'mysql',
 			'host'     => '127.0.0.1',
-			'database' => 'mfcl',
-			'username' => 'root',
-			'password' => 'price31',
+			'database' => 'database',
+			'username' => 'username',
+			'password' => 'password',
 			'charset'  => 'utf8',
 			'prefix'   => '',
 		),
